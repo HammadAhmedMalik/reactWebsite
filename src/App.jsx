2e46285cce80d67455f51290cb1bc7b797af7d6f
HammadAhmedMalik <hammadahmedmalik64@gmail.com>
2024-12-12 15:06:40 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import Header from './Header.jsx'
 function App() {
   return(
 
-    <header></header>
+    <header>
+       <h1>My First Website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Services</li>
+                    <li><a href="#"></a>Contacts</li>
+                </ul>
+            </nav>
+            <hr></hr>
+    </header>
 
 
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,15 +3,7 @@ function Header(){
     return(
 
         <header>
-            <h1>My First Website</h1>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Services</li>
-                    <li>Contacts</li>
-                </ul>
-            </nav>
+           
         </header>
         
     );
